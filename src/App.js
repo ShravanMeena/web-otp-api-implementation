96a6c7b7af3c13b858d1e111d7fb060415a2dc60
shravanmeena47 <shravanmeena47@gmail.com>
2021-11-07 10:31:12 +0530

    web-otp-api full code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
 export default class App extends React.Component {
   state = {
     otp: "",
@@ -29,10 +29,32 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <h2>Your OTP is: {this.state.otp}</h2>
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <h1>The Web OTP API Docs</h1>
+
+        <div>
+          Send an SMS that includes
+          <pre>
+            <code>@wyourdomain #12345</code>
+          </pre>
+          at the last line to this phone.
+          <pre>
+            <code>please don't use https:// and / end of the url</code>
+          </pre>
+          <pre>
+            <code>example for use @www.google.com</code>
+          </pre>
+          <pre>
+            <code>example for not use XXXXX @https://www.google.com XXXXX</code>
+          </pre>
+        </div>
       </div>
     );
   }
 }
-
 
 // time to test in mobile phone
