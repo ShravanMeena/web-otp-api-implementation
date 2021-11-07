0ce19dfe4a84123b8b1336eabd4a46ebd9dd3a3e
shravanmeena47 <shravanmeena47@gmail.com>
2021-11-07 10:33:49 +0530

    web-otp-api full code

--- src/App.js
+++ src/App.js
@@ -28,27 +28,29 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <br />
+        <br />
+        <br />
+        <br />
         <h2>Your OTP is: {this.state.otp}</h2>
         <br />
         <br />
         <br />
         <br />
-
-        <h1>The Web OTP API Docs</h1>
-
+        <h3>The Web OTP API Docs</h3>
         <div>
           Send an SMS that includes
           <pre>
-            <code>@wyourdomain #12345</code>
+            <code>@web-otp-shravan.netlify.app #12345</code>
           </pre>
           at the last line to this phone.
           <pre>
             <code>please don't use https:// and / end of the url</code>
           </pre>
-          <pre>
+          <pre style={{ color: "green" }}>
             <code>example for use @www.google.com</code>
           </pre>
-          <pre>
+          <pre style={{ color: "red" }}>
             <code>example for not use XXXXX @https://www.google.com XXXXX</code>
           </pre>
         </div>
